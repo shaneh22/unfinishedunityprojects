104c865286351cbf5fba7bf2c884f031bfa53a15
shaneh22 <shaneh22@live.unc.edu>
2020-02-28 00:11:16 -0500

    New Game

---
+++ Lose Control/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody rb;
+
+    void Start()
+	{
+        rb = GetComponent<RigidBody2D>();
+	}
+}
