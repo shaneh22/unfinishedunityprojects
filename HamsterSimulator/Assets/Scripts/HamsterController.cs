f90c509d9d47266c09a6337d8f062ab447636b27
shaneh22 <shaneh22@live.unc.edu>
2020-10-04 02:14:16 -0400

    HamsterLoop

---
+++ HamsterSimulator/Assets/Scripts/HamsterController.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HamsterController : MonoBehaviour
+{
+    private Animator anim;
+    public readonly float cycleDistance = .2f * Mathf.PI; //8 inches in hamster wheel diameter * pi
+    public readonly Color BLACK = new Color(0, 0, 0);
+    public readonly Color RED = new Color(255, 0, 0);
+
+    public TMP_Text timeText;
+    public TMP_Text distanceText;
+    public TMP_Text totalDistanceText;
+    public TMP_Text levelText;
+    public TMP_Text cyclesText;
+    public TMP_Text speedText;
+    public TMP_Text availablePointsText;
+    public TMP_Text maxSpeedUpgradesText;
+    public TMP_Text accelerationUpgradesText;
+    public TMP_Text enduranceUpgradesText;
+    public Slider enduranceSlider;
+    public GameObject upgradeButton;
+    public GameObject upgradeScreen;
+    public GameObject upgradePointButtons;
+
+    private float startTime;
+    private float distanceTraveled;
+    private float totalDistanceTraveled;
+    private double maxSpeed = 3;
+    private float speedMultiplier = 1f;
+    private float acceleration = 0.01f;
+    private int endurance = 75;
+    private int cyclesSinceLevelUp;
+    private int cyclesBeforeLevelUp;
+    private int level = 1;
+    private int upgradesToUse;
+    private int maxSpeedUpgrades;
+    private int accelerationUpgrades;
+    private int enduranceUpgrades;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        upgradeScreen.SetActive(false);
+        upgradeButton.SetActive(false);
+        _ = StartCoroutine(AtRest());
+    }
+
+    private IEnumerator AtRest()
+    {
+        timeText.text = "0:00:00.00";
+        distanceTraveled = 0;
+        distanceText.text = "0 m";
+        speedText.text = "0";
+        speedText.color = BLACK;
+        enduranceSlider.maxValue = endurance;
+        enduranceSlider.value = 0;
+        while (true)
+        {
+            if(Input.anyKeyDown && Input.GetAxis("Horizontal") > 0)
+            {
+                anim.SetBool("isRunning", true);
+                _ = StartCoroutine(StartRunning());
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    private IEnumerator StartRunning()
+    {
+        _ = StartCoroutine(UpdateTimeText());
+        while (enduranceSlider.value < endurance)
+        {
+            if (Input.anyKeyDown && Input.GetAxis("Horizontal") > 0)
+            {
+                double speed = System.Math.Round(anim.speed * speedMultiplier, 2);
+                if (speed < maxSpeed)
+                {
+                    speedText.color = BLACK;
+                    speedMultiplier += acceleration;
+                    anim.SetFloat("SpeedMultiplier", speedMultiplier);
+                }
+                else
+                {
+                    speed = maxSpeed;
+                    speedText.color = RED;
+                }
+                speedText.text = speed + "";
+            }
+            enduranceSlider.value += Time.deltaTime * speedMultiplier;
+            
+            yield return null;
+        }
+        _ = StartCoroutine(Tired());
+    }
+
+    private IEnumerator UpdateTimeText()
+    {
+        startTime = Time.time;
+        while (anim.GetBool("isRunning"))
+        {
+            timeText.text = TimeToString(System.Math.Round(Time.time - startTime, 2));
+            yield return null;
+        }
+    }
+
+    private string TimeToString(double time)
+    {
+        int minutes = (int)(time / 60);
+        int hours = minutes / 60;
+        time -= minutes * 60;
+        minutes -= hours * 60;
+        if(time < 10)
+        {
+            return hours + ":" + minutes.ToString("D2") + ":0" + string.Format("{0:F2}", time);
+        }
+        return hours + ":" + minutes.ToString("D2") + ":" + string.Format("{0:F2}", time);
+    }
+
+    private IEnumerator Tired()
+    {
+        for (float i = speedMultiplier -.2f; i >= 0f; i -= .2f)
+        {
+            speedMultiplier = i;
+            speedText.text = System.Math.Round(anim.speed * speedMultiplier, 2) + "";
+            speedText.color = BLACK;
+            yield return new WaitForSeconds(.5f);
+        }
+        anim.SetBool("isRunning", false);
+        yield return new WaitForSeconds(2f);
+        _ = StartCoroutine(AtRest());
+    }
+
+    public void UpgradeAcceleration()
+    {
+        if (upgradesToUse > 0)
+        {
+            acceleration += .003f;
+            accelerationUpgrades++;
+            accelerationUpgradesText.text = accelerationUpgrades + "";
+            upgradesToUse--;
+            availablePointsText.text = "Available Points: " + upgradesToUse;
+            CheckIfPointsAvailable();
+        }
+    }
+
+    public void UpgradeMaxSpeed()
+    {
+        if (upgradesToUse > 0)
+        {
+            speedText.color = BLACK;
+            maxSpeed += 1;
+            maxSpeedUpgrades++;
+            maxSpeedUpgradesText.text = maxSpeedUpgrades + "";
+            upgradesToUse--;
+            availablePointsText.text = "Available Points: " + upgradesToUse;
+            CheckIfPointsAvailable();
+        }
+    }
+
+    public void UpgradeEndurance()
+    {
+        if (upgradesToUse > 0)
+        {
+            endurance += 50;
+            enduranceUpgrades++;
+            enduranceUpgradesText.text = enduranceUpgrades + "";
+            upgradesToUse--;
+            availablePointsText.text = "Available Points: " + upgradesToUse;
+            enduranceSlider.maxValue = endurance;
+            CheckIfPointsAvailable();
+        }
+    }
+
+    public void CompletedCycle()
+    {
+        distanceTraveled += cycleDistance;
+        totalDistanceTraveled += cycleDistance;
+        distanceText.text = Mathf.Round(distanceTraveled) + " m";
+        totalDistanceText.text = Mathf.Round(totalDistanceTraveled) + " m";
+        cyclesSinceLevelUp++;
+        cyclesBeforeLevelUp = (int)(10 + (10 * Mathf.Log10(level))) - cyclesSinceLevelUp;
+        cyclesText.text = cyclesBeforeLevelUp + "";
+        CheckIfLevelUp();
+    }
+
+    private void CheckIfLevelUp()
+    {
+        if (cyclesBeforeLevelUp == 0)
+        {
+            LevelUp();
+            levelText.text = level + "";
+            cyclesSinceLevelUp = 0;
+        }
+    }
+
+    private void LevelUp()
+    {
+        level++;
+        upgradesToUse++;
+        upgradeButton.SetActive(true);
+        CheckIfPointsAvailable();
+    }
+
+    public void OnUpgradeClick()
+    {
+        upgradeScreen.SetActive(true);
+        upgradePointButtons.SetActive(true);
+        availablePointsText.text = "Available Points: " + upgradesToUse;
+    }
+
+    public void CloseUpgradeWindow()
+    {
+        upgradeScreen.SetActive(false);
+    }
+
+    public void CheckIfPointsAvailable()
+    {
+        if(upgradesToUse == 0)
+        {
+            upgradePointButtons.SetActive(false);
+            upgradeButton.SetActive(false);
+        }
+        else
+        {
+            upgradePointButtons.SetActive(true);
+            availablePointsText.text = "Available Points: " + upgradesToUse;
+        }
+    }
+}
+
